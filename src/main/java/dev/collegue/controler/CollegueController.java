cb8275e3f0c7e6f8515eebbfd8f0483948f964fe
Nicolas <marty.nicolas11@laposte.net>
2019-04-18 12:11:04 +0200

    Ajout d'un message en cas de succès d'une création d'un collaborateur

--- src/main/java/dev/collegue/controler/CollegueController.java
+++ src/main/java/dev/collegue/controler/CollegueController.java
@@ -3,6 +3,8 @@ package dev.collegue.controler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,9 +48,10 @@ public class CollegueController {
 	}
 	
 	@PostMapping
-	public void ajouterCollegue(@RequestBody Collegue collegue) {
+	public ResponseEntity<String> ajouterCollegue(@RequestBody Collegue collegue) {
 		
 		service.sauvegardeCollegue(collegue);
+		 return ResponseEntity.status(HttpStatus.OK).body("Le nouveau collègue a bien été ajouté");
 
 	}
 	
