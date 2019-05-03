58fba32d8bef9fa4da90a68da71c4db8dd9dd123
Nicolas <marty.nicolas11@laposte.net>
2019-05-03 09:52:06 +0200

    Mise à jour des messages des requêtes HTTP

--- src/main/java/dev/collegue/exception/MyExceptionHandler.java
+++ src/main/java/dev/collegue/exception/MyExceptionHandler.java
@@ -1,5 +1,6 @@
 package dev.collegue.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,13 +12,13 @@ public class MyExceptionHandler {
 	@ExceptionHandler(value = { CollegueNonTrouveException.class })
 	protected ResponseEntity<Object> handleConflictCollegueNotFound(RuntimeException ex, WebRequest request) {
 		String bodyOfResponse = "Collegue non trouvé";
-	    return ResponseEntity.status(404).body(bodyOfResponse);
+	    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(bodyOfResponse);
 	 }
 	
 	@ExceptionHandler(value = { CollegueInvalideException.class })
 	protected ResponseEntity<Object> handleConflictCollegueInvalid(RuntimeException ex, WebRequest request) {
 		String bodyOfResponse = "Collègue invalide: " + CollegueInvalideException.message;
-	    return ResponseEntity.status(400).body(bodyOfResponse);
+	    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bodyOfResponse);
 	 }
 	
 }
