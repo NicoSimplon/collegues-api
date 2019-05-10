4f52256110376d1a8a6c88f093a48dbb69422ebf
Nicolas <33360039+NicoSimplon@users.noreply.github.com>
2019-05-10 07:16:22 +0200

    bug: problème cross origin request access denied

--- src/main/java/dev/collegue/controler/CollegueController.java
+++ src/main/java/dev/collegue/controler/CollegueController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -35,6 +36,7 @@ import dev.collegue.utils.DtoUtils;
  *
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/collegues")
 public class CollegueController {
 
